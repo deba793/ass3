6c684fd9871235fea6a1486c1cfb611810e195ba
deba793 <debasisrout793@gmail.com>
2023-07-12 15:18:35 +0530

    assignment-3

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asssignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string mystring;
+            int result;
+            Console.WriteLine("Enter the string :");
+            mystring = Console.ReadLine();
+            if (int.TryParse(mystring, out result))
+            {
+                Console.WriteLine("converted integer is \t" + result);
+            }
+            else
+            {
+                Console.WriteLine("invalid converstion");
+            }
+        
+    }
+    }
+}
